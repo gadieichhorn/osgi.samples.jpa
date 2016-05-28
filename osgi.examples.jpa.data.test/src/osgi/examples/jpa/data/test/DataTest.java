b49fc3195b7b9400cf836824401be722bc3a4bed
gadieichhorn <gadi.eichhorn@gmail.com>
2016-05-28 09:05:31 +0100

    implement integration tests

--- osgi.examples.jpa.data.test/src/osgi/examples/jpa/data/test/DataTest.java
+++ osgi.examples.jpa.data.test/src/osgi/examples/jpa/data/test/DataTest.java
@@ -4,6 +4,9 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.FrameworkUtil;
+import org.osgi.util.tracker.ServiceTracker;
+
+import osgi.examples.jpa.data.api.Data;
 
 /**
  * 
@@ -13,12 +16,18 @@ public class DataTest {
 
     private final BundleContext context = FrameworkUtil.getBundle(this.getClass()).getBundleContext();
     
+    private     <T> T getService(Class<T> clazz) throws InterruptedException {
+    	ServiceTracker<T,T> st = new ServiceTracker<>(context, clazz, null);
+    	st.open();
+    	return st.waitForService(1000);
+    }    
+    
     /*
      * 
      */
     @Test
     public void testData() throws Exception {
-    	Assert.assertNotNull(context);
-    }
+    	Assert.assertNotNull(getService(Data.class));
+   	}
 
 }
